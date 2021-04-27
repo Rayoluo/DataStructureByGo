9fd2d85bf1cffabd178d2b481e31d1057b4b8985
oluoluo <hustly123@gmail.com>
2021-04-27 15:30:13 +0800

    add implement of ci68

---
+++ 04BinarySearchTree/Problem-Set/codingInterviews68/main.go
@@ -0,0 +1,33 @@
+package main
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	// if root.Val <= max(p.Val, q.Val) && root.Val >= min(p.Val, q.Val) {
+	// 	return root
+	// }
+	return root
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
